de5aa7669cee0c49fa1d438f6a4421ee1c470435
Mbah  naoussi jordan wilfry <jordanwilfry02@gmail.com>
2023-07-11 11:24:48 +0100

    initial commit

---
+++ introduction.js
@@ -0,0 +1,42 @@
+/* Variables declarations **************************************************/
+
+// var initialVariable = null;
+// var myVariable = 10;
+// let myVariableName = "hello";
+// const variable = 12;
+
+// myVariable = 15;
+// myVariableName = "Good morning";
+
+// console.log();
+// console.log(myVariable);
+// console.log(myVariableName);
+// console.log(variable);
+
+/** Operators in javascript *****************************************************/
+//Addition , subtraction, multiplication, division, and modulo
+// var a = 2,
+//   b = 10;
+
+// console.log(a + b);
+
+// console.log(a - b);
+
+// console.log(a * b);
+
+// console.log(a / b);
+
+// console.log(a % b);
+
+
+/** increment and decrement *************************************************/
+// var increment = 4, decrement = 12;
+// console.log(increment, decrement)
+
+// increment = increment + 1;
+// increment++;
+// increment+=1;
+// decrement = decrement - 1;
+// decrement--;
+// decrement-=1;
+// console.log(increment, decrement)
