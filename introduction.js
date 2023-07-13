10a69df38277fe7148849b95627878dad99f8743
Mbah  naoussi jordan wilfry <jordanwilfry02@gmail.com>
2023-07-13 05:32:23 +0100

    calculator application implimentation

--- introduction.js
+++ introduction.js
@@ -28,7 +28,6 @@
 
 // console.log(a % b);
 
-
 /** increment and decrement *************************************************/
 // var increment = 4, decrement = 12;
 // console.log(increment, decrement)
@@ -41,8 +40,25 @@
 // decrement-=1;
 // console.log(increment, decrement)
 
+/** working on Strings *****************************************************************/
+// const string = "hello world"
 
-/** working on Strings */
-const string = "hello world"
+// console.log(string.length);
 
-console.log(string.length);
+/** Working on Array ***************************************************************/
+var variable = 1;
+var myArr = [23, 45, "hello", null, true, variable];
+console.log(myArr);
+// myArr.pop()
+// var last = myArr.push("hello")
+// myArr.shift();
+// myArr.unshift("bonjour")
+
+// myArr.map((i, j)=>{
+    // console.log("index: " + j + ", value: " + i);
+//     console.log(`index: ${j}, value: ${i}`);
+// })
+
+console.log(myArr.some((i)=>(i=true)))
+
+// console.log(myArr);
